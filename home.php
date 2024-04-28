f2336cbf2c36634fd8237f37ba6cdec5a030fa7e
Ahmad Afifudin <aviv.haltec@gmail.com>
2024-04-28 22:09:41 +0700

    mapel pilihan

---
+++ home.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+	<title>Home | User</title>
+</head>
+<body>
+<div class="navbar justify-content-end">
+	<ul class="nav justify-content-end">
+		<li class="nav-item">
+			<a class="nav-link active" aria-current="page" href="#">Active</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Link</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="#">Link</a>
+		</li>
+	</ul>
+</div>
+</body>
+</html>
